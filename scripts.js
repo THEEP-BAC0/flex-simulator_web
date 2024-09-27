57d84bc6c5fd7ce945f3694e51ccecc5becbb8b4
THEEP-BAC0 <181171831+THEEP-BAC0@users.noreply.github.com>
2024-09-26 18:55:33 -0700

    Created block customization setting

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 
 const blockBtn = document.getElementById("blockBtn");
 const container = document.getElementById("container");
-let block = document.getElementsByClassName("block");
 const justCont = document.getElementById("just-cont");
 const algnItems = document.getElementById("algn-items")
+const algnSelf = document.getElementById("algn-self")
 let blockVal = 0;
+let blockId = "";
 
-//
+// Code for when the block (create button) is click
 blockBtn.onclick = () => {
 
     // Add block value by 1
@@ -18,26 +19,43 @@ blockBtn.onclick = () => {
     block.classList.add("block");
     block.id = `block-${Date.now()}`
 
+    // Event Listener for block
+    block.addEventListener("click", function () {
+        let blockIdLabel = document.getElementById("blockId");
+        blockId = block.id;
+
+        console.log(block.id.slice(12));
+        blockIdLabel.innerText = `Block ID ${block.id.slice(12)}`;
+    })
+
     // Append new block to container
     container.appendChild(block);
 }
 
-// Code when block is clicked
-block.onclick = () => {
-    return;
-}
-
 
 // Event listener for justCont dropdown
-justCont.addEventListener("input", function () {
+justCont.addEventListener("change", function () {
     let just = justCont.value;
 
     container.style.justifyContent = just;
 })
 
 // Event listener for algnItems dropdown
-algnItems.addEventListener("input", function () {
+algnItems.addEventListener("change", function () {
     let algn = algnItems.value;
 
     container.style.alignItems = algn;
 })
+
+// Event listener for algnSelf dropdown
+algnSelf.addEventListener("input", function () {
+    let algn = algnSelf.value;
+
+    // Select the block using the latest blockId
+    if (blockId) {
+        const currentBlock = document.getElementById(blockId);
+        if (currentBlock) {
+            currentBlock.style.alignSelf = algn;
+        }
+    }
+});
